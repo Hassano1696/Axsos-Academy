3229fc099bd0fb5e28cdc7cf5595cfacd4054eb8
hassano1696 <hassanodeh1997@gmail.com>
2022-01-15 14:11:14 +0200

    commit

---
+++ JS/Loop Challenges/Decreasing/Decreasing.js
@@ -0,0 +1,5 @@
+for (var i = 100; i >= 1; i -= 1) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
---
+++ JS/Loop Challenges/Factorial/Factorial.js
@@ -0,0 +1,5 @@
+var fac=1;
+for (var i =1; i<=12; i+=1){
+    fac=fac*i;
+}
+console.log(fac);
