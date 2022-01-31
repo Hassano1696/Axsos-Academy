0a99b7bd2ac6c2d7a71bf0705f2cfd117075466f
hassano1696 <hassanodeh1997@gmail.com>
2022-01-31 03:18:53 +0200

    commit

--- python_stack/_python/python_fundamentals/Function_intedmediate/Fun_inter1.py
+++ python_stack/_python/python_fundamentals/Function_intedmediate/Fun_inter1.py
@@ -1,6 +1,6 @@
 import random
 def randInt(min=  0 , max=  100 ):
-    if min > max or max<0:
+    if min >= max or max<=0:
         return False
     else:
         num = round((random.random()*(max-min))+min)
@@ -9,4 +9,4 @@ print(randInt())
 print(randInt(max=50)) 
 print(randInt(min=50)) 	
 print(randInt(min=50, max=500))   
-print(randInt(min=100, max=2))  #it will return false immediately 
+print(randInt(min=20, max=2))  #it will return false immediately 
