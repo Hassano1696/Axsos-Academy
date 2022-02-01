3649dbad57526bdf4975c922aa066103bcd4f54d
hassano1696 <hassanodeh1997@gmail.com>
2022-02-01 15:04:19 +0200

    commit

--- python_stack/_python/OOP/user/user.py
+++ python_stack/_python/OOP/user/user.py
@@ -5,18 +5,30 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):
         self.account_balance += amount
-        print('User:',self.name,',','Balance:',self.account_balance,'$')
     def make_withdrawal(self, amount):	
         self.account_balance -= amount
-        print('User:',self.name,',','Balance:',self.account_balance,'$')
     def  Transfer(self, anotheruser, amount) :
         self.account_balance -= amount
         anotheruser.account_balance += amount
-        print('User:',self.name,',','Balance:',self.account_balance,'$')
-        print('User:',anotheruser.name,',','Balance:',anotheruser.account_balance,'$')
     def display_user_balance(self):
         print('User:',self.name,',','Balance:',self.account_balance,'$')
 RJ = User('Ramos Jerry','dijegoeg@gmai.com')
 Mike = User('Mike Jordan','cflkgdhildjfg@facebook.com')
 Nafez= User('Nafez Mahfouz','fdbdojg@cuw.com')
-
+RJ.make_deposit(200)
+RJ.make_deposit(300)
+RJ.make_deposit(20000)
+RJ.make_withdrawal(19500)
+RJ.display_user_balance()
+Mike.make_deposit(200)
+Mike.make_deposit(50)
+Mike.make_withdrawal(120)
+Mike.make_withdrawal(80)
+Mike.display_user_balance()
+Nafez.make_deposit(2000)
+Nafez.make_withdrawal(70)
+Nafez.make_withdrawal(150)
+Nafez.make_withdrawal(400)
+Nafez.display_user_balance()
+RJ.Transfer(Nafez, 200)
+RJ.display_user_balance()
