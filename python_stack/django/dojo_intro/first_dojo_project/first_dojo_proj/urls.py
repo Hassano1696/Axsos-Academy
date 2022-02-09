8f3f21169880dc2bfa64f80eb02519e3a1c73f3d
hassano1696 <hassanodeh1997@gmail.com>
2022-02-09 07:11:31 +0200

    commit

--- python_stack/django/dojo_intro/first_dojo_project/first_dojo_proj/urls.py
+++ python_stack/django/dojo_intro/first_dojo_project/first_dojo_proj/urls.py
@@ -5,9 +5,8 @@ urlpatterns = [
     path('', views.root),
     path('blogs', views.index),
     path('blogs/new', views.new),
-    path('blogs/creat', views.creat),
+    path('blogs/create', views.create),
     path('blogs/<int:number>', views.show),
     path('blogs/<int:number>/edit', views.edit),
-    path('blogs/<int:number>/delete', views.delete),
+    path('blogs/<int:number>/delete', views.distroy),
 ]
-
---
+++ python_stack/django/dojo_intro/ninja_project/ninja_app/views.py
@@ -0,0 +1,7 @@
+import re
+from django.shortcuts import render,redirect
+
+def main(request):
+    return render(request,'ninja.html')
+    
+
