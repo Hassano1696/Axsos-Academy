0e17ee197c9917433e0decb025dc4fbd2646ebb2
hassano1696 <hassanodeh1997@gmail.com>
2022-02-12 15:06:35 +0200

    commit

--- python_stack/django/django_orm/users_temp/users_temp_app/urls.py
+++ python_stack/django/django_orm/users_temp/users_temp_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.survey),
-   # path('store', views.store),	   
+    path('store', views.store),	   
 ]
--- python_stack/django/django_orm/users_temp/users_temp_app/views.py
+++ python_stack/django/django_orm/users_temp/users_temp_app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 
+
+from django.shortcuts import render, redirect
+from .models import User
 def survey(request):
-    return render(request,'index.html')
+    context = {
+        "users": User.objects.all()
+    }
+    return render(request, "index.html", context)
 
 
-#def store(request):
+def store(request):
+    new_user = User(first_name=request.POST['firstname'],last_name=request.POST['lastname'],email=request.POST['email'],age=request.POST['age'])
+    new_user.save()
+    return redirect("/")
