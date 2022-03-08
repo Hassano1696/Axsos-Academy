862f7e27648d8e1dfc9aad25ddbff229b6622635
hassano1696 <hassanodeh1997@gmail.com>
2022-03-08 16:14:39 +0300

    commit

---
+++ Java_Stack/oop/ZooKeeper/src/zookeeper/GorillaTest.java
@@ -0,0 +1,17 @@
+package zookeeper;
+class GorillaTest {
+    public static void main(String[] args) {
+    	Gorilla gorilla= new Gorilla();
+    	gorilla.throwSomething();
+    	gorilla.throwSomething();
+    	gorilla.throwSomething();
+    	gorilla.eatbanana();
+    	gorilla.eatbanana();
+    	gorilla.climb();
+
+
+
+    }
+
+
+}
