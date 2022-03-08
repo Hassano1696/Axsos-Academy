9794c1fc44b4024a1cd54f4a2bacbbc63b0303f6
hassano1696 <hassanodeh1997@gmail.com>
2022-03-08 15:58:57 +0300

    commit

---
+++ Java_Stack/Fundamentals/hashmap/HashMapp.java
@@ -0,0 +1,17 @@
+import java.util.Set;
+import java.util.HashMap;
+
+public class HashMapp {
+    public  static  void main( String [] args) {
+        HashMap<String,String> trackList =  new  HashMap <String,String>();
+        trackList .put( "Astronut In The Ocean" , "Masked Wolf " );
+        trackList .put( "My Immortal" , "Evancensce" );
+        trackList .put( "Sorry" , "Halsey" );
+        trackList .put( "Unstoppable" , "Sia" );
+        System .out.println(trackList.get("Unstoppable"));  
+        for( String key : trackList.keySet()) {
+            System .out.println(key);
+            System .out.println(trackList.get(key));    
+        }
+    }
+}
