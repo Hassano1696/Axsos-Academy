54dc0b3d25de8927e7d9bebd4ef0bd3f08ca0fde
hassano1696 <hassanodeh1997@gmail.com>
2022-03-13 17:01:50 +0300

    commit

---
+++ Java_Stack/Spring/Display Date/displaydate/src/main/java/com/example/demo/DisplaydateApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+
+public class DisplaydateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplaydateApplication.class, args);
+	}
+
+	
+	@RequestMapping("/")
+	public String main() {
+		return ("index.jsp");
+	}
+	@RequestMapping("/date")
+	public String date() {
+		return ("date.jsp");
+	}
+	@RequestMapping("/time")
+	public String time() {
+		return ("time.jsp");
+	}
+
+}
