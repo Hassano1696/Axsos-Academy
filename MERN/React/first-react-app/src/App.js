00762cdbe0202aef9fb99338a737ad49f36d13b6
hassano1696 <hassanodeh1997@gmail.com>
2022-04-10 15:04:42 +0300

    commit

---
+++ MERN/React/first-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row my-5">
+          <div className="col-4 text-start">
+            <h1>Hello Dojo!</h1>
+            <h3>Things I need to do:</h3>
+            <ul>
+              <li>Learn React</li>
+              <li>Climb Mt. Everest</li>
+              <li>Run a Marathon</li>
+              <li>Feed the dogs</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
