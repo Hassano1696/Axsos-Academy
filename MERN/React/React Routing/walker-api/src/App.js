44527e50d63bf8696e627f69e563469b6052b626
hassano1696 <hassanodeh1997@gmail.com>
2022-04-26 14:40:33 +0300

    commit

--- MERN/React/React Routing/walker-api/src/App.js
+++ MERN/React/React Routing/walker-api/src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import Form from "./components/Form";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import People from "./components/People";
-import Planet from "./components/Planet";
 import Star from "./components/Star";
+import Planet from "./components/Planet";
 
 function App() {
+  
+
+  
   return (
     /*render the form in the same page as the results*/
     <BrowserRouter>
       <div className="container my-5">
-        <Form /> 
+        <Form />
         <Routes>
           <Route path="/people/:id" element={<People />} />
           <Route path="/planets/:id" element={<Planet />} />
-          <Route path="/starships/:id" element={<Star />} />
+          <Route path="/star/:id" element={<Star />} />
         </Routes>
       </div>
     </BrowserRouter>
